3d2d2abebb3856ba138e749914e5667301690e22
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-07-06 10:50:10 -0400

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AuthModule } from "src/auth/auth.module";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { MatGridListModule } from "@angular/material/grid-list";
 import { NavbarComponent } from "./layout/navbar/navbar.component";
 
 @NgModule({
@@ -14,6 +19,11 @@ import { NavbarComponent } from "./layout/navbar/navbar.component";
     AuthModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
