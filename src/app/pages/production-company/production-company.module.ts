4e23e42756a8b0b8e32332c53463bce4660c9317
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-07-08 11:03:10 -0400

    production-company module

---
+++ src/app/pages/production-company/production-company.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductionCompanyRoutingModule } from './production-company-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ProductionCompanyRoutingModule
+  ]
+})
+export class ProductionCompanyModule { }
