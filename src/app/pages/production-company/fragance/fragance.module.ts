9c3446a52ea609c41b38a7024e180da52c175a41
RHRivasG <51801113+RHRivasG@users.noreply.github.com>
2020-07-08 12:41:46 -0400

    production company modules

---
+++ src/app/pages/production-company/fragance/fragance.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FraganceRoutingModule } from './fragance-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    FraganceRoutingModule
+  ]
+})
+export class FraganceModule { }
